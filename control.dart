ae8032f056d77e31d6a34a410064f400bfcad60b
Gathimba <gathimbawachira@gmail.com>
2024-03-30 10:51:59 +0300

    first commit

---
+++ control.dart
@@ -0,0 +1,16 @@
+//Dart control flow assignment 
+void main() {
+  var z = 10;
+  if(z>10)
+  {
+    print("yor number is greater than 10");
+  } 
+   else if(z == 10)
+  {
+    print("your number is less than 10");
+  }
+  else{
+    print("your number is equals to 10");
+  }
+  
+}
